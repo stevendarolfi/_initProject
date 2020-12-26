b55c19060bda2760c27009f66539d8c07010fa0a
steven.d'arolfi@d2l.com <>
2020-12-26 03:32:10 -0500

    add component structure to component template js file

---
+++ component/_template_componentName/componentName.js
@@ -0,0 +1,96 @@
+/***
+NAME
+****/
+app.component.NAME = {};
+/***
+OBJS
+****/
+app.component.NAME.objs = [];
+/******
+SETTING
+*******/
+app.component.NAME.setting = {};
+/****
+STATE
+*****/
+app.component.NAME.state = {};
+/***
+FUNC
+****/
+app.component.NAME.func = {};
+/*******************
+data CRUDS functions
+********************/
+app.component.NAME.func.create     = {}
+app.component.NAME.func.createSet  = {};
+app.component.NAME.func.retrieve   = {};
+app.component.NAME.func.update     = {};
+app.component.NAME.func.delete     = {}
+app.component.NAME.func.set        = {};
+/******************
+view CRUD functions
+*******************/
+app.component.NAME.func.make       = {};
+app.component.NAME.func.makeAppend = {};
+app.component.NAME.func.get        = {};
+app.component.NAME.func.give       = {};
+app.component.NAME.func.remove     = {};
+app.component.NAME.func.append     = {};
+/**************
+other functions
+***************/
+app.component.NAME.func.event      = {};
+app.component.NAME.func.init       = {};
+app.component.NAME.func.is         = {};
+app.component.NAME.func.sort       = {};
+app.component.NAME.func.transition = {};
+
+
+/***********
+func hotkeys
+************
+
+********************
+data CRUDS functions
+********************
+CREATE
+
+CREATESET
+
+RETRIEVE
+
+UPDATE
+
+DELETE
+
+SET
+
+********************
+view CRUDS functions
+********************
+MAKE
+
+MAKEAPPEND
+
+GET
+
+GIVE
+
+REMOVE
+
+APPEND
+
+***************
+other functions
+***************
+EVENT
+
+INIT
+
+IS
+
+SORT
+
+TRANSITION
+
+*/
